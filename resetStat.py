77f7142f1258177555155a6cddd74cc46ccaec94
Nusab Taha <nusabtaha33@gmail.com>
2023-10-03 22:17:26 +0600

    reset data

--- resetStat.py
+++ resetStat.py
@@ -1,3 +1,3 @@
 from sys import argv
 from pymongo import MongoClient
-MongoClient(argv[-1])["Projects"]["Contest-Hive"].update_one({"_id": 1}, {"$set": {"past24": 0,"past24api": 0,"past24page": 0}})
+MongoClient(argv[-1])["Projects"]["contest-hive"].update_one({"_id": 1}, {"$set": {"past24": 0,"past24api": 0,"past24page": 0}})
