ea3d5928c880e28f01b02a57686f48affd43c2fa
Nusab Taha <nusabtaha33@gmail.com>
2024-06-22 04:58:56 +0600

    fix: Handle invalid start time for contests in sendToTelegram.py

--- cache/sendToTelegram.py
+++ cache/sendToTelegram.py
@@ -86,6 +86,10 @@ for platform, contests in data.items():
         title = cleanText(title)
         duration = readableTime(duration)
         utcStartTime = startTime
+        if isinstance(startTime, int):
+            print(f"Invalid start time for {title} from {platform} {startTime}.")
+            continue
+    
         then = datetime.datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%SZ").replace(
             tzinfo=datetime.timezone.utc
         )
