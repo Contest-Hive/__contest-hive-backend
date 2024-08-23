f238d44f87cfd9f6f6ed47492b9638ee6c11c52c
Nusab Taha <nusabtaha33@gmail.com>
2024-08-24 01:05:11 +0600

    update daily stats every day

--- resetStat.py
+++ resetStat.py
@@ -1,3 +1,49 @@
+"""
+This script updates the daily statistics and resets the daily statistics to 0.
+
+"""
+
+import os
 from sys import argv
+from dotenv import load_dotenv
 from pymongo import MongoClient
-MongoClient(argv[-1])["Projects"]["contest-hive"].update_one({"_id": 1}, {"$set": {"past24": 0,"past24api": 0,"past24page": 0}})
+from datetime import datetime, timezone, timedelta
+
+load_dotenv()
+
+MongoURI = argv[-1]
+
+if len(argv) < 2:
+    MongoURI = os.environ.get("MongoURI")
+    print("Using the default MongoDB URI from .env file")
+
+db = MongoClient(MongoURI)["Projects"]
+utcDate = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+# 2024-08-23
+
+# check if the date is already in the database
+if db["daily-stats"].find_one({"_id": utcDate}):
+    print("Adding to the next date.")
+    # add to the next day
+    utcDate = (datetime.now(timezone.utc) + timedelta(days=1)).strftime("%Y-%m-%d")
+
+
+# get the current statistics
+stats = db["contest-hive"].find_one({"_id": 1})
+# update the daily statistics
+db["daily-stats"].insert_one(
+    {
+        "_id": utcDate,
+        "past24api": stats["past24api"],
+        "past24page": stats["past24page"],
+    }
+)
+
+# reset the daily statistics
+db["contest-hive"].update_one(
+    {"_id": 1}, {"$set": {"past24": 0, "past24api": 0, "past24page": 0}}
+)
+
+print(f"Reset the daily statistics for {utcDate}.")
+print(f"past24api: {stats['past24api']}")
+print(f"past24page: {stats['past24page']}")
